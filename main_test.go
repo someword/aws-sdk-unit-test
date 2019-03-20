00ed7fc79bc713a1a2ff1fcdaf07715f7ead4652
Derek Olsen <someword@gmail.com>
2019-03-19 19:08:11 -0700

    mroe

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -20,75 +19,56 @@ func (m *mockEC2Client) DescribeInstances(input *ec2.DescribeInstancesInput) (*e
 }
 
 func TestGetEKSEC2InstanceHostname(t *testing.T) {
-
-	instances := []*ec2.Instance{
-		&ec2.Instance{
-			State:      &ec2.InstanceState{Name: aws.String(ec2.InstanceStateNameRunning)},
-			InstanceId: aws.String(fmt.Sprintf("i-%s", "i1")),
-			Tags: []*ec2.Tag{
-				{
-					Key:   aws.String("Service"),
-					Value: aws.String("whizbang"),
-				},
-				{
-					Key:   aws.String("Team"),
-					Value: aws.String("seahawks"),
-				},
-			},
-			NetworkInterfaces: []*ec2.InstanceNetworkInterface{{PrivateDnsName: aws.String("ip-1.1.1.1.us-west-2.computer.internal")}},
-		},
-		&ec2.Instance{
-			State:      &ec2.InstanceState{Name: aws.String(ec2.InstanceStateNameStopped)},
-			InstanceId: aws.String(fmt.Sprintf("i-%s", "i2")),
-			Tags: []*ec2.Tag{
-				{
-					Key:   aws.String("Role"),
-					Value: aws.String("Kubernetes"),
-				},
-				{
-					Key:   aws.String("Team"),
-					Value: aws.String("seahawks"),
-				},
-			},
-		},
-		&ec2.Instance{
-			State:      &ec2.InstanceState{Name: aws.String(ec2.InstanceStateNameRunning)},
-			InstanceId: aws.String(fmt.Sprintf("i-%s", "i3")),
-			Tags: []*ec2.Tag{
-				{
-					Key:   aws.String("Role"),
-					Value: aws.String("Kubernetes"),
-				},
-				{
-					Key:   aws.String("Team"),
-					Value: aws.String("rams"),
-				},
-			},
-		},
-	}
-
-	r := &ec2.DescribeInstancesOutput{
-		Reservations: []*ec2.Reservation{
-			&ec2.Reservation{
-				Instances: instances,
-			},
-		},
-	}
 	tests := []struct {
-		name   string
-		Output *ec2.DescribeInstancesOutput
-		Error  error
+		name, expected string
+		Output         *ec2.DescribeInstancesOutput
+		Error          error
 	}{
-		{"test 1", r, nil},
-		{"test 2", &ec2.DescribeInstancesOutput{}, nil},
+		{"test 1", "ip-1.1.1.1.us-west-2.computer.internal", &ec2.DescribeInstancesOutput{
+			Reservations: []*ec2.Reservation{
+				{
+					Instances: []*ec2.Instance{
+						{
+							State: &ec2.InstanceState{Name: aws.String(ec2.InstanceStateNameStopped)},
+							Tags: []*ec2.Tag{
+								{
+									Key:   aws.String("Service"),
+									Value: aws.String("whizbang"),
+								},
+								{
+									Key:   aws.String("Team"),
+									Value: aws.String("seahawks"),
+								},
+							},
+							NetworkInterfaces: []*ec2.InstanceNetworkInterface{{PrivateDnsName: aws.String("ip-2.2.2.2.us-west-2.computer.internal")}},
+						},
+						{
+							State: &ec2.InstanceState{Name: aws.String(ec2.InstanceStateNameRunning)},
+							Tags: []*ec2.Tag{
+								{
+									Key:   aws.String("Service"),
+									Value: aws.String("whizbang"),
+								},
+								{
+									Key:   aws.String("Team"),
+									Value: aws.String("seahawks"),
+								},
+							},
+							NetworkInterfaces: []*ec2.InstanceNetworkInterface{{PrivateDnsName: aws.String("ip-1.1.1.1.us-west-2.computer.internal")}},
+						},
+					},
+				},
+			},
+		}, nil},
+		{"test 2", "blaj", &ec2.DescribeInstancesOutput{}, nil},
 	}
 
 	for _, test := range tests {
 		m := &mockEC2Client{Output: test.Output}
 		c := EC2Client{svc: m}
-		h := c.GetHost()
-		fmt.Printf("test name is %s\n", test.name)
-		fmt.Println(h)
-
+		actual := c.GetHost()
+		if actual != test.expected {
+			t.Errorf("GetHost(): test name: %s expected: %s actual: %s\n", test.name, test.expected, actual)
+		}
 	}
 }
