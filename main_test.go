ad62e7bf0c5b809ffcb1c4a9237d7bb6a51cc648
Derek Olsen <someword@gmail.com>
2019-03-19 18:54:27 -0700

    works

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 )
@@ -19,12 +20,66 @@ func (m *mockEC2Client) DescribeInstances(input *ec2.DescribeInstancesInput) (*e
 }
 
 func TestGetEKSEC2InstanceHostname(t *testing.T) {
+
+	instances := []*ec2.Instance{
+		&ec2.Instance{
+			State:      &ec2.InstanceState{Name: aws.String(ec2.InstanceStateNameRunning)},
+			InstanceId: aws.String(fmt.Sprintf("i-%s", "i1")),
+			Tags: []*ec2.Tag{
+				{
+					Key:   aws.String("Service"),
+					Value: aws.String("whizbang"),
+				},
+				{
+					Key:   aws.String("Team"),
+					Value: aws.String("seahawks"),
+				},
+			},
+			NetworkInterfaces: []*ec2.InstanceNetworkInterface{{PrivateDnsName: aws.String("ip-1.1.1.1.us-west-2.computer.internal")}},
+		},
+		&ec2.Instance{
+			State:      &ec2.InstanceState{Name: aws.String(ec2.InstanceStateNameStopped)},
+			InstanceId: aws.String(fmt.Sprintf("i-%s", "i2")),
+			Tags: []*ec2.Tag{
+				{
+					Key:   aws.String("Role"),
+					Value: aws.String("Kubernetes"),
+				},
+				{
+					Key:   aws.String("Team"),
+					Value: aws.String("seahawks"),
+				},
+			},
+		},
+		&ec2.Instance{
+			State:      &ec2.InstanceState{Name: aws.String(ec2.InstanceStateNameRunning)},
+			InstanceId: aws.String(fmt.Sprintf("i-%s", "i3")),
+			Tags: []*ec2.Tag{
+				{
+					Key:   aws.String("Role"),
+					Value: aws.String("Kubernetes"),
+				},
+				{
+					Key:   aws.String("Team"),
+					Value: aws.String("rams"),
+				},
+			},
+		},
+	}
+
+	r := &ec2.DescribeInstancesOutput{
+		Reservations: []*ec2.Reservation{
+			&ec2.Reservation{
+				Instances: instances,
+			},
+		},
+	}
 	tests := []struct {
 		name   string
 		Output *ec2.DescribeInstancesOutput
 		Error  error
 	}{
-		{"test 1", &ec2.DescribeInstancesOutput{}, nil},
+		{"test 1", r, nil},
 		{"test 2", &ec2.DescribeInstancesOutput{}, nil},
 	}
 
